35d7971f1eb14325f0fbcf7f2c6e851f0446853d
Muhammad Yahya <zulfikeryahya@gmail.com>
2023-09-23 02:00:22 +0600

    commit responsive navbarBottom

--- src/Shared/Navbar/NavBarTop/NavBarTop.jsx
+++ src/Shared/Navbar/NavBarTop/NavBarTop.jsx
@@ -7,9 +7,9 @@ const NavBarTop = () => {
             <input className='w-full sm:py-1 md:py-2 rounded-lg' type="text" name="search"/>
             <button type='submit' className='text-background border-2 border-solid border-primary bg-primary sm:py-1 md:py-2 sm:px-3 md:px-5 rounded-lg absolute right-0 top-0 bottom-0'>Search</button>
          </div>
-         <div className='sm:hidden md:flex md:items-centertext-slate-300 md:text-sm'>
-            <p>call: 8801625290235</p>
-            <p className='pl-5'>email: hmyahya@gmail.com</p>
+         <div className='sm:hidden lg:block flex items-center text-slate-300 text-sm'>
+            <p className='inline'>call: 8801625290235</p>
+            <p className='pl-5 inline'>email: hmyahya@gmail.com</p>
          </div>
       </div>
    );
--- src/Shared/Navbar/NavbarBottom/NavBarBottom.jsx
+++ src/Shared/Navbar/NavbarBottom/NavBarBottom.jsx
@@ -1,8 +1,49 @@
-
+import { FaCartPlus } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 const NavBarBottom = () => {
+   const categories = [
+      'Categories',
+      'Sports Car',
+      'Truck',
+      'Regular Car',
+      'Fire Truck',
+      ' Police Car',
+   ]
+   const menu = [
+      'Home',
+      'All Toys',
+      'My Toys',
+      'Add A Toy',
+      'Blogs',
+   ]
    return (
-      <div>
-         <h2>NavBar Bottom</h2>
+      <div className="flex items-center justify-between sm:px-10 md:px-16 sm:2 md:py-2 bg-primary">
+         <div className="w-60">
+            <select className="select select-bordered w-full max-w-xs bg-background text-black md:text-xl sm:text-sm">
+               {
+                  categories.map((category, i) => <option key={i} value={category}>{category}</option>)
+               }
+            </select>
+         </div>
+         <div className="sm:hidden lg:block">
+            <ul className="flex me-36 text-white text-xl font-medium">
+               {
+                  menu.map((link, i) => <li className="px-3 py-2 cursor-pointer" key={i}>{link}</li>)
+               }
+            </ul>
+         </div>
+         <div className="flex">
+            <div className="relative">
+            <FaCartPlus className="text-background text-2xl mt-2 z-10"/>
+            <span className="absolute -mt-10 ml-4 text-white text-xl font-medium">0</span>
+            </div>
+            <div className="avatar online sm:hidden lg:block ml-10">
+               <div className="w-10 rounded-full">
+                  <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+               </div>
+            </div>
+            <div className="lg:hidden ml-10"><FaBars className="text-2xl mt-1 text-background"/></div>
+         </div>
       </div>
    );
 };
